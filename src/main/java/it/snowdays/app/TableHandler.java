00e32031a2d60bd9b612223072f98ab3e8fa75db
Unknown <riki.1@hotmail.it>
2018-01-17 15:06:14 +0100

    Table handler update

---
+++ src/main/java/it/snowdays/app/TableHandler.java
@@ -0,0 +1,16 @@
+package it.snowdays.app;
+
+/**
+ * TableHandler
+ */
+public class TableHandler {
+    
+    public TableHandler(){
+
+    }
+
+    public static void setTableView(){
+        
+    }
+
+}
