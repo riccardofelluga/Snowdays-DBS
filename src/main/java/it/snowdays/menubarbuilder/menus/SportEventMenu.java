b88fc35707c82edf42c7a714e334d02fa8e4d076
Unknown <francois.macaire@gmail.com>
2018-01-27 15:26:49 +0100

    Shit reguarding scoreboard display

--- src/main/java/it/snowdays/menubarbuilder/menus/SportEventMenu.java
+++ src/main/java/it/snowdays/menubarbuilder/menus/SportEventMenu.java
@@ -1,6 +1,8 @@
 package it.snowdays.menubarbuilder.menus;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Optional;
 
 import it.snowdays.app.DataHandler;
 import it.snowdays.app.panels.ViewPane;
@@ -13,8 +15,24 @@ import javafx.scene.control.MenuItem;
  */
 public class SportEventMenu extends Menu{
 
-    private String locQuery = "";
-    private String staffQuery = "";
+    private String locQuery = "SELECT se.description, l.address, l.name " + 
+                                " FROM (sport_event se LEFT JOIN is_played_at ipa ON se.sport_event_id = ipa.sport_event_id)" +
+                                " LEFT JOIN location l ON ipa.location_id = l.location_id";
+    
+    private String staffQuery = "SELECT p.name, p.surname, se.description AS sport_event, l.address AS location " +
+                                " FROM (((staff s LEFT JOIN participant p ON s.stud_id = p.stud_id) " +
+                                " RIGHT JOIN put_together pt ON pt.stud_id = s.stud_id " +
+                                " LEFT JOIN sport_event se ON pt.sport_event_id = se.sport_event_id) " +
+                                " LEFT JOIN is_played_at ipa ON se.sport_event_id = ipa.sport_event_id) " +
+                                " LEFT JOIN location l ON ipa.location_id = l.location_id";
+    
+    private int sportEventId;
+    private String competition;
+    private String retrieveId = "SELECT se.sport_event_id FROM sport_event se WHERE se.description = \'" + competition + "\'";
+
+    private String scoreboardSingle = "SELECT ci.placement, p.name, p.surname, p.university " +
+                                        " FROM competes_in ci LEFT JOIN participant p ON ci.stud_id = p.stud_id " +
+                                        " WHERE sport_event_id = " + sportEventId + " ORDER BY placement ASC";
     
     public SportEventMenu(){
         super("Sport Events");
@@ -33,9 +51,9 @@ public class SportEventMenu extends Menu{
 
         MenuItem scoreboard = new MenuItem("Scoreboard");
         scoreboard.setOnAction(e -> {
-            String ID = getSBID();
-            DataHandler.getInstance().loadRemote("SELECT * FROM " + ID);
-            ViewPane.getInstance().updateView();
+            sportEventId = getSBID();
+            DataHandler.getInstance().loadRemote(scoreboardSingle);
+            ViewPane.getInstance().updateView();//
         });
 
         getItems().add(scoreboard);
@@ -43,20 +61,26 @@ public class SportEventMenu extends Menu{
         getItems().add(manageSportStaff);
     }
 
-    private String getSBID(){
+    private int getSBID(){
 
-        HashMap<String, String> sKeys  = new HashMap<String, String>();
-        sKeys.put("", "");//insert here the mapping
+        ArrayList<String> events  = new ArrayList<String>();
+        events.add("Ski Race");
+        events.add("Beer Pong");
+        events.add("Tiro cinnio");
+        events.add("Giro al Sas");
 
         ChoiceDialog<String> d = new ChoiceDialog<String>();
         d.setTitle("Choose scoreboard...");
         d.setHeaderText("Choose the scoreboard you want to see");
         d.setContentText("Scoreboard: ");
-        d.getItems().addAll(sKeys.values());
+        d.getItems().addAll(events);
         //d.setSelectedItem(""); //set the default selection!
 
-        d.showAndWait();
-
-        return "staff"; // returns the id of the table
+        Optional<String> result = d.showAndWait();
+        if (result.isPresent()){
+            competition = result.get();
+        }
+        sportEventId = loadRemote(retrieveId);
+        return sportEventId; // returns the id of the table
     }
 }
