3b48b34017b5a113bffbde24c40e3f646bec6a6e
Unknown <francois.macaire@gmail.com>
2018-01-29 09:54:26 +0100

    Colored all the menus

--- src/main/java/it/snowdays/menubarbuilder/MenuBarBuilder.java
+++ src/main/java/it/snowdays/menubarbuilder/MenuBarBuilder.java
@@ -37,6 +37,9 @@ public class MenuBarBuilder extends MenuBar{
         mb.getMenus().add(new EdibleMenu());
         mb.getMenus().add(new BaseCampMenu());
 
+        mb.setStyle("-fx-selection-bar: #000080;");
+        mb.setStyle("-fx-background-color: #87cefa;");
+
         return mb;
     }
     public static MenuBar sponsorMenu(){
@@ -44,6 +47,9 @@ public class MenuBarBuilder extends MenuBar{
         
         mb.getMenus().add(new SponsorMenu());
         mb.getMenus().add(new EdibleMenu());
+
+        mb.setStyle("-fx-selection-bar: #000080;");
+        mb.setStyle("-fx-background-color: #87cefa;");
 
         return mb;
     }
@@ -53,6 +59,9 @@ public class MenuBarBuilder extends MenuBar{
         mb.getMenus().add(new ParticipantsMenu());
         mb.getMenus().add(new AccomodationsMenu());
         mb.getMenus().add(new HostsMenu());
+
+        mb.setStyle("-fx-selection-bar: #000080;");
+        mb.setStyle("-fx-background-color: #87cefa;");
        
         return mb;
     }
@@ -63,11 +72,17 @@ public class MenuBarBuilder extends MenuBar{
         mb.getMenus().add(new SportEventMenu());
         mb.getMenus().add(new HelperMenu());
 
+        mb.setStyle("-fx-selection-bar: #000080;");
+        mb.setStyle("-fx-background-color: #87cefa;");
+
         return mb;
     }
 
     public static MenuBar cateringMenu(){
         MenuBar mb = new MenuBar();
+
+        mb.setStyle("-fx-selection-bar: #000080;");
+        mb.setStyle("-fx-background-color: #87cefa;");
 
         mb.getMenus().add(new HelperMenu());
         mb.getMenus().add(new EventMenu());
