aba9e90de9a3909eecfe60cf421726d9cc812b50
Unknown <riki.1@hotmail.it>
2018-01-30 16:26:42 +0100

    MINOR "GLITCH"
    prof

--- src/main/java/it/snowdays/app/AddHandler.java
+++ src/main/java/it/snowdays/app/AddHandler.java
@@ -235,9 +235,9 @@ public class AddHandler {
 
     private static boolean manageTasks(){
         boolean r1, r2;
-        r1 = SQLFetcher.nonSelectQuery("INSERT INTO task(task_id, description, start_hour, end_hour) VALUES ('" + collectedData.get(0) + "' , '" + collectedData.get(1)+ "' , '" + collectedData.get(2) + "' , '" + collectedData.get(3) + "')");
-        r2 = SQLFetcher.nonSelectQuery("INSERT INTO must_help_in(stud_id, task_id) VALUES ('"+ collectedData.get(4) + "' , '" + collectedData.get(0) + "')");
-        return r1 && r2;
+        //r1 = SQLFetcher.nonSelectQuery("INSERT INTO task(task_id, description, start_hour, end_hour) VALUES ('" + collectedData.get(0) + "' , '" + collectedData.get(1)+ "' , '" + collectedData.get(2) + "' , '" + collectedData.get(3) + "')");
+        r2 = SQLFetcher.nonSelectQuery("INSERT INTO must_help_in(stud_id, task_id) VALUES ('"+ collectedData.get(0) + "' , '" + collectedData.get(1) + "')");
+        return r2;
     }
 
     private static boolean insertInTeamSportsScoreboard()
