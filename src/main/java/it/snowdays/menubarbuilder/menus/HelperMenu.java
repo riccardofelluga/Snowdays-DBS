3b51b051912b756f3e64dca6b5271cd6b5b1ee6c
Unknown <riki.1@hotmail.it>
2018-01-30 11:43:19 +0100

    Added other readonly cols

--- src/main/java/it/snowdays/menubarbuilder/menus/HelperMenu.java
+++ src/main/java/it/snowdays/menubarbuilder/menus/HelperMenu.java
@@ -2,24 +2,25 @@ package it.snowdays.menubarbuilder.menus;
 
 import it.snowdays.app.DataHandler;
 import it.snowdays.app.panels.ViewPane;
+import java.util.ArrayList;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 
 public class HelperMenu extends Menu{
     //private String assignQuery = "SELECT t.task_id, t.description, t.start_hour, t.end_hour, m.stud_id FROM task t LEFT JOIN must_help_in m ON t.task_id = m.task_id ORDER BY task_id";
     private String personsQuery = "SELECT  p.stud_id, p.surname, p.name, s.role FROM participant p RIGHT JOIN staff s ON p.stud_id = s.stud_id ORDER BY p.surname";
-    private String tasksQuery = "SELECT t.task_id, t.description, t.start_hour, t.end_hour, p.stud_id FROM ((task t RIGHT JOIN must_help_in m ON t.task_id = m.task_id) LEFT JOIN helper h ON m.stud_id  = h.stud_id) LEFT JOIN participant p ON h.stud_id = p.stud_id";
+    private String tasksQuery = "SELECT t.task_id, t.description, t.start_hour, t.end_hour, p.name, p.surname, p.stud_id FROM ((task t RIGHT JOIN must_help_in m ON t.task_id = m.task_id) LEFT JOIN helper h ON m.stud_id  = h.stud_id) LEFT JOIN participant p ON h.stud_id = p.stud_id";
 
     public HelperMenu(){
         super("Helper");
-        // MenuItem assigStaff = new MenuItem("Assign to staff");
-        // assigStaff.setOnAction(e -> {
-        //     DataHandler.getInstance().loadRemote(assignQuery, "assignToStaff");
-        //     ViewPane.getInstance().updateView();
-        // });
+       
         MenuItem manageTasks = new MenuItem("Assign to staff");
         manageTasks.setOnAction(e -> {
             DataHandler.getInstance().loadRemote(tasksQuery, "manageTasks");
+            ArrayList<String> a = new ArrayList<String>();
+            a.add("name");
+            a.add("surname");
+            DataHandler.getInstance().setReadCols(a);
             ViewPane.getInstance().updateView();
         });
 
